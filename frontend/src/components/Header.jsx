977053f0335ec8a86136b083c0d5d2647dd14755
flklr-dev <kitadriand@gmail.com>
2024-12-07 21:03:30 +0800

    j

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { FaHeart, FaBell, FaChevronLeft } from 'react-icons/fa';
 import api from '../config/axios';
-import logo from './uploads/pantrypals.png';
+import logo from '/images/pantrypals.png';
 
 const Header = () => {
   const navigate = useNavigate();
